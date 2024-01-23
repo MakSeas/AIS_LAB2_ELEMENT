f1b146c102539b9d9f3452235860021bc5b76b8b
MakSeas <koshmus2003@yandex.ru>
2024-01-23 19:42:59 +0700

    Добавьте файлы проекта.

---
+++ AIS_LAB2_ELEMENT/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS_LAB2_ELEMENT
+{
+    public class Detail 
+    {
+        public int code;
+        public string name;
+        public string material;
+        public double mass;
+
+        public Detail(int code, string name, string material, double mass) 
+        { 
+            this.code = code;
+            this.name = name;
+            this.material = material;
+            this.mass = mass;
+        }
+    }
+
+}
